33df94af0276e96cc0ac02cf9c84761e361222e1
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-03-21 13:50:14 -0700

    SDLColors
    This works and is included in the app build

---
+++ database/SDLColors.h
@@ -0,0 +1,160 @@
+#ifndef SDLCOLORS
+#define SDLCOLORS
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+// Basic Colors
+extern const SDL_Color cBlack;
+extern const SDL_Color cGray;
+extern const SDL_Color cRed_Light;
+extern const SDL_Color cRed;
+extern const SDL_Color cGreen_Light;
+extern const SDL_Color cGreen;
+extern const SDL_Color cYellow_Light;
+extern const SDL_Color cYellow;
+extern const SDL_Color cBlue_Light;
+extern const SDL_Color cBlue;
+extern const SDL_Color cMagenta_Light;
+extern const SDL_Color cMagenta;
+extern const SDL_Color cCyan_Light;
+extern const SDL_Color cCyan;
+extern const SDL_Color cWhite;
+
+// Custom Red Shades
+extern const SDL_Color IndigenousRed;
+extern const SDL_Color LightCoral;
+extern const SDL_Color Salmon;
+extern const SDL_Color DarkSalmon;
+extern const SDL_Color LightSalmon;
+extern const SDL_Color Crimson;
+extern const SDL_Color FireBrick;
+
+// Custom Pink Shades
+extern const SDL_Color Pink;
+extern const SDL_Color LightPink;
+extern const SDL_Color HotPink;
+extern const SDL_Color DeepPink;
+extern const SDL_Color MediumVioletRed;
+extern const SDL_Color PaleVioletRed;
+
+// Custom Orange Shades
+extern const SDL_Color orangeLightSalmon;
+extern const SDL_Color Coral;
+extern const SDL_Color Tomato;
+extern const SDL_Color OrangeRed;
+extern const SDL_Color Orange;
+
+// Custom Yellow Shades
+extern const SDL_Color Gold;
+extern const SDL_Color LightYellow;
+extern const SDL_Color LemonChiffon;
+extern const SDL_Color LightGoldenrodYellow;
+extern const SDL_Color PapayaWhip;
+extern const SDL_Color Moccasin;
+extern const SDL_Color PeachPuff;
+extern const SDL_Color PaleGoldenrod;
+extern const SDL_Color Khaki;
+extern const SDL_Color Dark_Khaki;
+
+// Custom Purple Shades
+extern const SDL_Color Lavender;
+extern const SDL_Color Thistle;
+extern const SDL_Color Plum;
+extern const SDL_Color Violet;
+extern const SDL_Color Orchid;
+extern const SDL_Color MediumOrchid;
+extern const SDL_Color MediumPurple;
+extern const SDL_Color RebeccaPurple;
+extern const SDL_Color BlueViolet;
+extern const SDL_Color DarkViolet;
+extern const SDL_Color DarkMagenta;
+extern const SDL_Color Purple;
+extern const SDL_Color Indigo;
+extern const SDL_Color SlateBlue;
+extern const SDL_Color DarkSlateBlue;
+
+// Custom Green Shades
+extern const SDL_Color GreenYellow;
+extern const SDL_Color Chartreuse;
+extern const SDL_Color LawnGreen;
+extern const SDL_Color LimeGreen;
+extern const SDL_Color PaleGreen;
+extern const SDL_Color LightGreen;
+extern const SDL_Color MediumSpringGreen;
+extern const SDL_Color SpringGreen;
+extern const SDL_Color MediumSeaGreen;
+extern const SDL_Color SeaGreen;
+extern const SDL_Color ForestGreen;
+extern const SDL_Color Green;
+extern const SDL_Color DarkGreen;
+extern const SDL_Color YellowGreen;
+extern const SDL_Color OliveDrab;
+extern const SDL_Color Olive;
+extern const SDL_Color DarkOliveGreen;
+extern const SDL_Color MediumAquamarine;
+extern const SDL_Color DarkSeaGreen;
+extern const SDL_Color LightSeaGreen;
+extern const SDL_Color DarkCyan;
+extern const SDL_Color Teal;
+
+//Custom Blue
+extern const SDL_Color LightCyan;
+extern const SDL_Color PaleTurqoise;
+extern const SDL_Color Aquamarine;
+extern const SDL_Color Turquoise;
+extern const SDL_Color MediumTurquoise;
+extern const SDL_Color DarkTurquoise;
+extern const SDL_Color CadetBlue;
+extern const SDL_Color SteelBlue;
+extern const SDL_Color MediumSlateBlue;
+extern const SDL_Color LightSteelBlue;
+extern const SDL_Color PowderBlue;
+extern const SDL_Color LightBlue;
+extern const SDL_Color SkyBlue;
+extern const SDL_Color LightSkyBlue;
+extern const SDL_Color DeepSkyBlue;
+extern const SDL_Color DodgerBlue;
+extern const SDL_Color CornflowerBlue;
+extern const SDL_Color RoyalBlue;
+extern const SDL_Color MediumBlue;
+extern const SDL_Color DarkBlue;
+extern const SDL_Color Navy;
+extern const SDL_Color MidnightBlue;
+
+//Custom Brown
+extern const SDL_Color Cornsilk;
+extern const SDL_Color BlanchedAlmond;
+extern const SDL_Color Bisque;
+extern const SDL_Color NavajoWhite;
+extern const SDL_Color Wheat;
+extern const SDL_Color BurlyWood;
+extern const SDL_Color Tan;
+extern const SDL_Color RosyBrown;
+extern const SDL_Color SandyBrown;
+extern const SDL_Color Goldenrod;
+extern const SDL_Color DarkGoldenrod;
+extern const SDL_Color Peru;
+extern const SDL_Color Chocolate;
+extern const SDL_Color SaddleBrown;
+extern const SDL_Color Sienna;
+extern const SDL_Color Brown;
+extern const SDL_Color Maroon;
+
+extern const SDL_Color Snow;
+extern const SDL_Color HoneyDew;
+extern const SDL_Color MintCream;
+extern const SDL_Color Azure;
+extern const SDL_Color AliceBlue;
+extern const SDL_Color GhostWhite;
+extern const SDL_Color WhiteSmoke;
+extern const SDL_Color SeaShell;
+extern const SDL_Color Beige;
+extern const SDL_Color OldLace;
+extern const SDL_Color FloralWhite;
+extern const SDL_Color Ivory;
+extern const SDL_Color AntiqueWhite;
+extern const SDL_Color Linen;
+extern const SDL_Color LavenderBlush;
+extern const SDL_Color MistyRose;
+#endif
